4d45578211843593c2d7bdcdac40d291f324b270
sioodmy <hello@sioodmy.dev>
2024-01-06 21:13:26 +0100

    feat: even more libre

--- src/search/libre.rs
+++ src/search/libre.rs
@@ -1,9 +1,20 @@
 use std::collections::HashMap;
+use crate::search::libre::Alternative::{Single, Multiple};
+use rand::seq::SliceRandom; 
+
+#[derive(Clone)]
+enum Alternative {
+    Single(&'static str),
+    Multiple(Vec<&'static str>)
+}
 
 lazy_static! {
-    static ref UNFREE: HashMap<&'static str, &'static str> = vec![
-        ("en.wikipedia.org", "wikiless.org"),
-        ("imgur.com", "rimgo.projectsegfau.lt")
+    static ref UNFREE: HashMap<&'static str, Alternative> = vec![
+        ("en.wikipedia.org", Single("wikiless.org")),
+        ("imgur.com", Single("rimgo.projectsegfau.lt")),
+        ("medium.com", Single("scribe.rip")),
+        ("tekstowo.pl", Single("davilarek.github.io/TekstoLibre/?")),
+        ("stackoverflow.com", Multiple(vec!["code.whatever.social", "ao.vern.cc", "overflow.fascinated.cc", "ao.owo.si", "a.opnxng.com"]))
     ]
     .into_iter()
     .collect();
@@ -11,7 +22,12 @@ lazy_static! {
 pub fn libre(url: String) -> String {
     let mut new = url;
     for (unfree, libre) in UNFREE.clone() {
-        new = new.replacen(unfree, libre, 1);
+        let instance = match libre {
+            Alternative::Single(instance)=> instance,
+            Alternative::Multiple(instances) =>
+            instances.choose(&mut rand::thread_rng()).unwrap()
+        };
+        new = new.replacen(unfree, instance, 1);
     }
     new
 }
