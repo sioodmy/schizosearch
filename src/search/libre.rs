0910933c548eba31002e800662349db770ba0f09
sioodmy <hello@sioodmy.dev>
2024-01-07 13:34:08 +0100

    fix: remove libre macro

--- src/search/libre.rs
+++ src/search/libre.rs
@@ -1,39 +1,22 @@
-use rand::seq::SliceRandom;
 use std::collections::HashMap;
 
-#[derive(Clone)]
-enum Alternative {
-    Single(&'static str),
-    Multiple(Vec<&'static str>),
-}
-
-//FIXME
-macro_rules! libre{
-    ($unfree:literal to $($libre:literal),+) => {
-        ($unfree, Alternative::Multiple(vec![$($libre),+]))
-    };
-    ($unfree:literal to $libre:literal) => {
-        ($unfree, Alternative::Single($libre))
-    }
-}
-
 lazy_static! {
-    static ref UNFREE: HashMap<&'static str, Alternative> = vec![
-        libre!("medium.com" to "scripe.com"),
-        libre!("en.wikipedia.org" to "wikiless.org"),
-        libre!("imgur.com" to "rimgo.projectsegfau.lt"),
-        libre!("tekstowo.pl" to "davilarek.github.io/TekstoLibre/?"),
-        libre!("stackoverflow.com" to "code.whatever.social", "ao.vern.cc", "overflow.fascinated.cc", "ao.owo.si", "a.opnxng.com")
-    ].into_iter().collect();
+    static ref UNFREE: HashMap<&'static str, &'static str> = vec![
+        ("en.wikipedia.org", "wikiless.org"),
+        ("imgur.com", "rimgo.projectsegfau.lt"),
+        ("medium.com", "scribe.rip"),
+        ("tekstowo.pl", "davilarek.github.io/TekstoLibre/?"),
+        ("stackoverflow.com", "code.whatever.social")
+    ]
+    .into_iter()
+    .collect();
 }
+
+/// Replace some unfree websites with libre frontends
 pub fn libre(url: String) -> String {
     let mut new = url;
     for (unfree, libre) in UNFREE.clone() {
-        let instance = match libre {
-            Alternative::Single(instance) => instance,
-            Alternative::Multiple(instances) => instances.choose(&mut rand::thread_rng()).unwrap(),
-        };
-        new = new.replacen(unfree, instance, 1);
+        new = new.replacen(unfree, libre, 1);
     }
     new
 }
--- src/search/special/definition.rs
+++ src/search/special/definition.rs
@@ -5,13 +5,16 @@ use tokio::sync::mpsc::Sender;
 
 use super::SpecialResult;
 
+// TODO
+// get definition for first word in query,
+// which doesnt start with "what", "is", "a", "an" etc
+
 pub async fn definition(query: &str, tx: Sender<SpecialResult>) -> Result<()> {
     let json = fetch!("https://api.dictionaryapi.dev/api/v2/entries/en/{}", query);
 
     let data: Value = serde_json::from_str(&json)?;
 
     if let Some(definition) = data[0]["meanings"][0]["definitions"][0]["definition"].as_str() {
-        println!("{}", definition);
         tx.send(SpecialResult::Definition(definition.to_owned()))
             .await
             .unwrap();
