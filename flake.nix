aa34fb2df796b036bf44f2af587cbb3436d659d0
sioodmy <hello@sioodmy.dev>
2024-04-28 21:41:01 +0200

    feat(nix): add treefmt

--- flake.nix
+++ flake.nix
@@ -4,11 +4,13 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      imports = [inputs.treefmt-nix.flakeModule];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
         packages = rec {
           schizosearch = pkgs.callPackage ./default.nix {};
@@ -21,6 +23,19 @@
             go
           ];
         };
+          treefmt.config = {
+      projectRootFile = "flake.nix";
+
+      programs = {
+        alejandra.enable = true;
+        deadnix.enable = true;
+        gofumpt.enable = true;
+        prettier.enable = true;
+        statix.enable = true;
+      };
+
+      settings.formatter.prettier.options = ["--tab-width" "4"];
+    };
       };
     };
 }
